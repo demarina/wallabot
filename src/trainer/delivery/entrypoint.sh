848cb950c87b1799818e465845b5c9e2b06c3bbb
duqueitor <adrian.urjc@gmail.com>
2025-11-16 14:12:45 +0100

    clean error messages

--- src/trainer/delivery/entrypoint.sh
+++ src/trainer/delivery/entrypoint.sh
@@ -2,8 +2,12 @@
 set -e
 
 echo "Generating dataset..."
-
 python ner_cli.py --task generate --data-folder-path /app/data
+echo "Dataset generated"
+
+
+echo "Start NER training..."
 python ner_cli.py --task train --data-folder-path /app/data --nlp-model-path /app/data
+echo "Finish NER training"
 
 exec "$@"
--- src/wallabot/wallabot.py
+++ src/wallabot/wallabot.py
@@ -1,3 +1,5 @@
+import os.path
+
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
@@ -11,9 +13,9 @@ from utils.mongo_db import MongoDB
 from src.trainer.ner import Ner
 
 OFFERS_URL = (
-    "https://es.wallapop.com/search?"
-    "distance_in_km=50&time_filter=lastMonth&min_sale_price=500&max_sale_price=2000&"
-    "keywords=bicicleta+orbea&order_by=price_high_to_low&source=side_bar_filters"
+    'https://es.wallapop.com/search?'
+    'distance_in_km=50&time_filter=lastMonth&min_sale_price=500&max_sale_price=2000&'
+    'keywords=bicicleta+orbea&order_by=price_high_to_low&source=side_bar_filters'
 )
 COLLECTION_NAME = 'walla_bikes'
 
@@ -21,11 +23,12 @@ COLLECTION_NAME = 'walla_bikes'
 def main():
     # Configuración del navegador (sin mensajes molestos)
     chrome_options = Options()
-    chrome_options.add_argument("--incognito")
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument('--start-maximized')
 
-    walla_ner = Ner(nlp_model_path='/Users/duqueitor/PycharmProjects/wallabot/src/trainer/data')
+    nlp_model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'trainer', 'data')
+    walla_ner = Ner(nlp_model_path=nlp_model_path)
     mongo_cli = MongoDB(host='localhost', port='27017', db_name='wallabot', user='admin', password='admin123')
 
     driver = webdriver.Chrome(
@@ -42,31 +45,34 @@ def main():
             EC.element_to_be_clickable((By.XPATH, "//button[contains(@id,'onetrust-accept-btn-handler')]"))
         )
         accept_button.click()
-        print("✅ Cookies aceptadas.")
+        print('Cookies aceptadas.')
     except TimeoutException:
-        print("⚠️ No apareció el banner de cookies (puede que ya estuviera aceptado).")
+        print('No apareció el banner de cookies (puede que ya estuviera aceptado).')
 
     # --- Esperar a que se carguen las tarjetas ---
     try:
-        wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "item-card_ItemCard--vertical__CNrfk")))
-        cards = driver.find_elements(By.CLASS_NAME, "item-card_ItemCard--vertical__CNrfk")
-        print(f"Se encontraron {len(cards)} productos.")
+        wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'item-card_ItemCard--vertical__CNrfk')))
+        cards = driver.find_elements(By.CLASS_NAME, 'item-card_ItemCard--vertical__CNrfk')
+        print(f'Se encontraron {len(cards)} productos.')
     except TimeoutException:
-        print("No se cargaron productos.")
+        print('No se cargaron productos.')
         driver.quit()
         return
 
     for e in cards:
         try:
-            title = e.find_element(By.CLASS_NAME, "item-card_ItemCard__title__5TocV").text
-            price = e.find_element(By.CLASS_NAME, "item-card_ItemCard__price__pVpdc").text
-            link = e.tag_name == "a" and e.get_attribute("href") or e.find_element(By.TAG_NAME, "a").get_attribute(
-                "href")
+            title = e.find_element(By.CLASS_NAME, 'item-card_ItemCard__title__5TocV').text
+            price = e.find_element(By.CLASS_NAME, 'item-card_ItemCard__price__pVpdc').text
+            link = e.tag_name == 'a' and e.get_attribute('href') or e.find_element(By.TAG_NAME, 'a').get_attribute(
+                'href')
             id = link.split('-')[-1]
 
-            features = walla_ner.predict(query=title)
+            try:
+                features = walla_ner.predict(query=title)
+            except Exception as e:
+                raise ValueError(f'Error predicting features in title "{title}": {e}')
 
-            print(f"{title} | {price} | {features}")
+            print(f'{title} | {price} | {features}')
 
             bike_info = {
                 'title': title,
@@ -76,15 +82,18 @@ def main():
                 'id': id
             }
 
-            mongo_cli.insert(coll_name=COLLECTION_NAME, data=bike_info)
+            try:
+                mongo_cli.insert(coll_name=COLLECTION_NAME, data=bike_info)
+            except Exception as e:
+                raise ValueError(f'Error inserting element "{bike_info}" in db: {e}')
 
-        except Exception as err:
-            print(f"Error procesando una tarjeta: {err}")
+        except Exception as e:
+            print(f'Error processing card: {e}')
             continue
 
-    print("\nFin del scraping.")
+    print('Scraping finish')
     driver.quit()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
