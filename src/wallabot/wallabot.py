1708a2204aa3a33acd3b8c58f8df48b1f570d2f2
duqueitor <adrian.urjc@gmail.com>
2025-11-28 13:00:25 +0100

    extract more info and call deepseek

--- src/wallabot/wallabot.py
+++ src/wallabot/wallabot.py
@@ -1,7 +1,9 @@
 import os.path
+import time
 
+import requests
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -12,16 +14,43 @@ from webdriver_manager.chrome import ChromeDriverManager
 from utils.mongo_db import MongoDB
 from src.trainer.ner import Ner
 
+from openai import OpenAI
+
 OFFERS_URL = (
     'https://es.wallapop.com/search?'
     'distance_in_km=50&time_filter=lastMonth&min_sale_price=500&max_sale_price=2000&'
     'keywords=bicicleta+orbea&order_by=price_high_to_low&source=side_bar_filters'
 )
 COLLECTION_NAME = 'walla_bikes'
+DOWNLOAD_IMAGES_FOLDER_PATH = ''  # COMPLETE
+DEEP_SEEK_API_KEY = 'sk-a0283572fde3497ca8ca12e6ee8daf0c'
+
+client = OpenAI(
+    api_key=DEEP_SEEK_API_KEY,
+    base_url='https://api.deepseek.com/v1'
+)
+
+
+def chat_with_deepseek(description: str) -> str:
+    try:
+        response = client.chat.completions.create(
+            model='deepseek-chat',
+            messages=[
+                {'role': 'system', 'content': 'Eres un asistente experto en crear anuncios para bicicletas.'},
+                {'role': 'user', 'content': f'Reformula el siguiente anuncio de manera concisa: {description}'}
+            ],
+            stream=False
+        )
+
+        return response.choices[0].message.content
+
+    except Exception as e:
+        print(f'Error calling Deepseek api: {e}')
+
+    return ''
 
 
 def main():
-    # Configuración del navegador (sin mensajes molestos)
     chrome_options = Options()
     chrome_options.add_argument('--incognito')
     chrome_options.add_argument('--disable-blink-features=AutomationControlled')
@@ -38,8 +67,6 @@ def main():
     driver.get(OFFERS_URL)
 
     wait = WebDriverWait(driver, 10)
-
-    # --- Aceptar cookies ---
     try:
         accept_button = wait.until(
             EC.element_to_be_clickable((By.XPATH, "//button[contains(@id,'onetrust-accept-btn-handler')]"))
@@ -60,12 +87,63 @@ def main():
         return
 
     for e in cards:
+        link = e.tag_name == 'a' and e.get_attribute('href') or e.find_element(
+            By.TAG_NAME, 'a').get_attribute('href')
         try:
             title = e.find_element(By.CLASS_NAME, 'item-card_ItemCard__title__5TocV').text
             price = e.find_element(By.CLASS_NAME, 'item-card_ItemCard__price__pVpdc').text
-            link = e.tag_name == 'a' and e.get_attribute('href') or e.find_element(By.TAG_NAME, 'a').get_attribute(
-                'href')
             id = link.split('-')[-1]
+
+            driver.execute_script(f"window.open('{link}', '_blank');")
+            tabs = driver.window_handles
+            driver.switch_to.window(tabs[1])
+            time.sleep(1)
+
+            complete_bike = driver.find_element(
+                By.CLASS_NAME, 'item-detail_ItemDetailTwoColumns__container__aOiE5')
+            last_updated = complete_bike.find_element(
+                By.CLASS_NAME, 'item-detail-stats_ItemDetailStats__description__015I3').text
+            views = complete_bike.find_element(By.CLASS_NAME, 'item-detail-stats_ItemDetailStats__ORKSL')
+            views_text = views.find_element(By.CLASS_NAME, 'd-flex').text
+            description = complete_bike.find_element(
+                By.CLASS_NAME, 'item-detail_ItemDetailTwoColumns__description__0DKb0').text
+            elemets = complete_bike.find_elements(
+                By.CLASS_NAME, 'item-detail-location_ItemDetailLocation__link__OmVsa'
+            )
+            location = elemets[0].text if elemets else None
+            state = complete_bike.find_element(
+                By.CLASS_NAME, 'item-detail-characteristics_ItemDetailCharacteristics__container__MDLJk').text
+            images_name = []
+
+            # Download images
+            try:
+                div_images = driver.find_element(By.CLASS_NAME, 'wallapop-carousel--rounded')
+                imgs = div_images.find_elements(By.TAG_NAME, "img")
+                for i, img in enumerate(imgs):
+                    url = img.get_attribute('src')
+
+                    extension = 'png'
+                    if '.jpg' in url:
+                        extension = 'jpg'
+
+                    name = f'{title.replace(" ", "_")}_{i}.{extension}'
+                    print(f'Downloading image {name} from url')
+
+                    response = requests.get(url)
+                    if response.status_code == 200:
+                        with open(name, 'wb') as f:
+                            f.write(response.content)
+                        images_name.append(name)
+
+                    else:
+                        print(f'Error downloading image {name}')
+
+            except Exception as e:
+                print(f'Error getting images {e}')
+
+            driver.close()  # close tab
+            driver.switch_to.window(tabs[0])
+            time.sleep(1)
 
             try:
                 features = walla_ner.predict(query=title)
@@ -79,7 +157,14 @@ def main():
                 'price': price,
                 'link': link,
                 'features': features,
-                'id': id
+                'id': id,
+                'last_updated': last_updated,
+                'views': views_text,
+                'description': description,
+                'location': location,
+                'state': state,
+                'images': images_name,
+                'new_description': chat_with_deepseek(description=description)
             }
 
             try:
@@ -87,8 +172,11 @@ def main():
             except Exception as e:
                 raise ValueError(f'Error inserting element "{bike_info}" in db: {e}')
 
+        except NoSuchElementException as e:
+            print(f'Element not fount {e}, in bike {link}.')
+            continue
         except Exception as e:
-            print(f'Error processing card: {e}')
+            print(f'Error processing card: {e}, in bike {link}.')
             continue
 
     print('Scraping finish')
